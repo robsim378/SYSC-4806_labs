c78a21a014c7b6fa29b3c5e69245b093b0b8fd99
Robert Simionescu <robsim378@gmail.com>
2023-10-16 13:57:46 -0400

    Added a couple more tests

--- src/test/java/AddressBookProgram/WebApplicationTests.java
+++ src/test/java/AddressBookProgram/WebApplicationTests.java
@@ -30,7 +30,17 @@ public class WebApplicationTests {
     }
 
     @Test
+    public void testGetAddressBooks() throws Exception {
+        this.mockMvc.perform(get("/getAddressBooks")).andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString("buddies")));
+    }
+
+    @Test
     public void testGetBuddy() throws Exception {
         this.mockMvc.perform(get("/getBuddy?addressBookId=1&buddyInfoId=1")).andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString("name"))).andExpect(content().string(containsString("house"))).andExpect(content().string(containsString("number")));
+    }
+
+    @Test
+    public void testGetBuddies() throws Exception {
+        this.mockMvc.perform(get("/getBuddies?addressBookId=1")).andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString("name"))).andExpect(content().string(containsString("house"))).andExpect(content().string(containsString("number")));
     }
 }
